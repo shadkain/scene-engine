f01df858f03bcd2b339bff0e717e0322da9e7c5f
shadkain <kryzev@gmail.com>
2020-01-26 20:28:55 +0300

    added EventBus

--- src/event/index.ts
+++ src/event/index.ts
@@ -1,2 +1,3 @@
 export * from './src/Box';
 export * from './src/BoxUser';
+export * from './src/Bus';
---
+++ src/event/src/Bus.ts
@@ -0,0 +1,43 @@
+export class Bus<K> {
+    private _channels: Map<K, Set<Function>>;
+
+    constructor() {
+        this._channels = new Map();
+    }
+
+    public on(key: K, handler: Function) {
+        let channel = this._channels.get(key);
+        if (!channel) {
+            channel = new Set();
+            this._channels.set(key, channel);
+        }
+
+        channel.add(handler);
+    }
+
+    public off(key: K, handler: Function) {
+        const channel = this._channels.get(key);
+        if (!channel) {
+            return;
+        }
+
+        channel.delete(handler);
+        if (!channel.size) {
+            this.dropChannel(key);
+        }
+    }
+
+    public emit(key: K, ...args: any[]) {
+        const channel = this._channels.get(key);
+        if (!channel) {
+            return;
+        }
+
+        channel.forEach(handler => handler(...args));
+    }
+
+    public dropChannel(key: K) {
+        this._channels.delete(key);
+    }
+
+}
