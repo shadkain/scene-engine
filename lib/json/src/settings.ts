8ba2999d7d80c23b5fba296c5889db709e2a7894
shadkain <kryzev@gmail.com>
2020-02-21 00:06:02 +0300

    documented json package settings fields

--- lib/json/src/settings.ts
+++ lib/json/src/settings.ts
@@ -2,19 +2,25 @@ export const settings = new class {
     private _tagKey: string;
     private _autoDetectDerived: boolean;
 
-    set tagKey(key: string) {
-        this._tagKey = key;
-    }
-
+    /**
+     * Json property key denoting a tag to determine derived object type.
+     */
     get tagKey(): string {
         return this._tagKey;
     }
 
-    set autoDetectDerived(flag: boolean) {
-        this._autoDetectDerived = flag;
+    set tagKey(key: string) {
+        this._tagKey = key;
     }
 
+    /** 
+     * Indicates whether enabling autodetecting derived objects by searching for tag key in object.
+     */
     get autoDetectDerived(): boolean {
         return this._autoDetectDerived;
     }
+
+    set autoDetectDerived(flag: boolean) {
+        this._autoDetectDerived = flag;
+    }   
 }
